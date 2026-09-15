9ae0e6318bf41e2d59207953ff4c6f4aad612090
Joao Pedro <joaopedrofernandes01@hotmail.com>
2026-09-14 21:14:55 -0300

    feat: add ui file with WeatherApp class and methods to display user interface

---
+++ ui.py
@@ -0,0 +1,134 @@
+from PyQt5.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
+from PyQt5.Qt import Qt
+from weather_service import WeatherService, WeatherServiceError
+
+
+class WeatherApp(QWidget):
+    def __init__(self):
+
+        super().__init__()
+
+        self.city_label = QLabel("Enter city name: ", self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton("Get Weather", self)
+        self.temperature_label = QLabel(self)
+        self.emoji_label = QLabel(self)
+        self.description_label = QLabel(self)
+
+    def initUI(self):
+
+        self.setWindowTitle("Weather App")
+
+        self.setFixedSize(400, 600)
+
+        vbox = QVBoxLayout()
+
+        vbox.addWidget(self.city_label)
+        vbox.addWidget(self.city_input)
+        vbox.addWidget(self.get_weather_button)
+        vbox.addWidget(self.temperature_label)
+        vbox.addWidget(self.emoji_label)
+        vbox.addWidget(self.description_label)
+
+        self.city_label.setAlignment(Qt.AlignCenter)
+        self.city_input.setAlignment(Qt.AlignCenter)
+        self.temperature_label.setAlignment(Qt.AlignCenter)
+        self.emoji_label.setAlignment(Qt.AlignCenter)
+        self.description_label.setAlignment(Qt.AlignCenter)
+
+        self.city_label.setObjectName("city_label")
+        self.city_input.setObjectName("city_input")
+        self.get_weather_button.setObjectName("get_weather_button")
+        self.temperature_label.setObjectName("temperature_label")
+        self.emoji_label.setObjectName("emoji_label")
+        self.description_label.setObjectName("description_label")
+
+        self.setLayout(vbox)
+
+        self.setStyleSheet("""
+            QLabel, QPushButton{
+                font-family: calibri;
+            }
+            QLabel#city_label{
+                font-size: 40px;
+                font-style: italic;
+            }
+            QLineEdit#city_input{
+                font-size: 40px;
+            }
+            QPushButton#get_weather_button{
+                font-size: 30px;
+                font-weight: bold;
+            }
+            QLabel#temperature_label{
+                font-size: 75px;
+            }
+            QLabel#emoji_label {
+                font-size: 100px;
+                font-family: "Segoe UI Emoji", "Noto Color Emoji", "Apple Color Emoji";
+            }
+            QLabel#description_label{
+                font-size: 50px;
+            }
+        """)
+
+        self.get_weather_button.clicked.connect(self.get_weather)
+
+    def get_weather(self):
+
+        city = self.city_input.text()
+
+        try:
+
+            data = WeatherService.fetch_data(city)
+            self.display_weather(data)
+
+        except WeatherServiceError as error:
+
+            self.display_error(str(error))
+
+    def display_error(self, message):
+
+        self.temperature_label.setStyleSheet("font-size: 30px")
+        self.temperature_label.setText(message)
+        self.emoji_label.clear()
+        self.description_label.clear()
+
+    def display_weather(self, data):
+        self.temperature_label.setStyleSheet("font-size: 75px")
+
+        temperature_k = data["main"]["temp"]
+        temperature_c = temperature_k - 273.15
+
+        weather_id = data["weather"][0]["id"]
+        weather_description = data["weather"][0]["description"]
+
+        self.temperature_label.setText(f"{temperature_c:.0f}ºC")
+        self.emoji_label.setText(self.get_weather_emoji(weather_id))
+        self.description_label.setText(weather_description)
+
+    @staticmethod
+    def get_weather_emoji(weather_id):
+        match weather_id:
+            case _ if 200 <= weather_id <= 232:
+                return "⛈️"
+            case _ if 300 <= weather_id <= 321:
+                return "🌥️"
+            case _ if 500 <= weather_id <= 531:
+                return "🌧️"
+            case _ if 600 <= weather_id <= 622:
+                return "🌨️"
+            case _ if 701 <= weather_id <= 741:
+                return "🌫"
+            case 762:
+                return "🌋"
+            case 771:
+                return "🌬️"
+            case 781:
+                return "🌪️"
+            case 800:
+                return "🌞"
+            case _ if 801 <= weather_id <= 804:
+                return "☁️"
+            case _:
+                return ""
