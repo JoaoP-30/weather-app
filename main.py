505d6353119f902c9332008f9ef0e0557634e879
Joao Pedro <joaopedrofernandes01@hotmail.com>
2026-09-14 22:34:06 -0300

    feat: add main file that executes the application

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from ui import WeatherApp
+
+def main():
+
+    app = QApplication(sys.argv)
+    weather_app = WeatherApp()
+    weather_app.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
